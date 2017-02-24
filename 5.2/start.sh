b787a0d19269fe96e75ca720fdb3e9468e69e7ca
hazim1093 <hazim_malik@hotmail.com>
2017-02-24 17:11:34 +0500

    Add resources for kibana v5.2

---
+++ 5.2/start.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# We just start the kibana service and then leave consul-template running on the foreground.
+# Here we use the CONSUL_URL environment variable that we defined before. Consul template
+# expects to find a kibana.ctmpl file in /templates. This is the template that we would
+# mount as a volume from our host. The result is then placed in /opt/kibana/config/kibana.yml
+# where kibana will be able to read from.
+
+# Note: & added so that kibana runs in a separate process and consul-template runs in this process
+kibana &
+consul-template -consul=$CONSUL_URL -template="/templates/kibana.ctmpl:/opt/kibana/config/kibana.yml:service kibana restart"
